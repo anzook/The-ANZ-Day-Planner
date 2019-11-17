04ad0488c8136341eee8913161d80f64020af623
anz213 <56008225+anzook@users.noreply.github.com>
2019-11-16 21:42:45 -0500

    local storage added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-$("#dateSelect").attr("value", moment().format('LL'));
+$("#dateSelect").attr("value", moment().format('MM/DD/YYYY'));
 $("#currentDay").text("Time: " +moment().format('LT'));
 $("#currentTime").text("Date: "+ moment().format('LL'));
+
+var toDos =[];
 
 function timerSet(){
     //math to get miliseconds until end of hour
@@ -13,8 +15,9 @@ function timerSet(){
 
 function setClass(time){  //return the class for the hour passed
 //gets current hour
-// var hours = moment().hours();
-var hours = 13;
+var hours = moment().hours();
+//test line below if needed
+// var hours = 13;  
 
 // console.log(hours);
 var timeH = parseInt(time);  //just in case
@@ -49,14 +52,67 @@ var timeDiv = $("<div>");
 var fieldDiv = $("<textarea>");
 var saveBtn = $("<button>");
 
-timeDiv.text(i + ":00").addClass("hour col-2");
+var hourDisp = 0;  //remove the below and set to 'i' to go to 24-hr time
+if ( i > 12 ) {
+ hourDisp = i - 12;
+} else ( hourDisp = i)
+var tempId = "hour" + i;
+timeDiv.text(hourDisp + ":00").addClass("hour col-2");
 fieldDiv.addClass("inputField col-8").addClass(setClass(i));
+fieldDiv.attr("id", tempId);
 saveBtn.addClass("saveBtn col-xs-2").text("Save");
 timePill.addClass("row");
 timePill.append(timeDiv).append(fieldDiv).append(saveBtn);
 // timePill.addClass("row")
 $("#main").append(timePill);
 }
+getFields();
 }
+
+//builds out the planner for the current day
+function getFields() {
+let dateSet = $("#dateSelect").val();
+dateSet = moment(dateSet).format("DDMMYYYY");
+toDos = JSON.parse(localStorage.getItem(dateSet));
+if (toDos != null) {
+for (var i = 9; i < 18; i++) {
+    var tempId = "#hour" + i;
+    let j = i - 9;
+    $(tempId).val(toDos[j]);
+}
+}
+console.log(dateSet);
+console.log(toDos);
+
+}
+
+
+//saves the planner as seen for the current day
+function setFields() {
+    let dateSet = $("#dateSelect").val();
+    dateSet = moment(dateSet).format("DDMMYYYY");
+    toDos = [];
+    // debugger;
+    for (var i = 9; i < 18; i++) {
+        var tempId = "#hour" + i;
+        let j = i - 9;
+        toDos[j] = $(tempId).val();
+        console.log($(tempId).val());
+    }
+    localStorage.setItem(dateSet, JSON.stringify(toDos));
+}
+
+$("#set-date-btn").on("click", function() {
+    event.preventDefault();
+getFields();
+});
+
+$("#current-date-btn").on("click", function() {
+    $(tempId).text();
+    $("#dateSelect").attr("value", moment().format('MM/DD/YYYY'));
+    });
+
+$(document).on("click", ".saveBtn", setFields);
+
 
 createFields();
