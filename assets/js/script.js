bad0a10d9e8572391a606eb131a9b873e0e66dd2
anz213 <56008225+anzook@users.noreply.github.com>
2019-11-19 18:55:48 -0500

    added Stetson save spec

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,14 +33,9 @@ if ( timeH < hours ){
 return classSet;  //returns class string
 };
 
-function setCurrent(){
-    //removeClass()
-    //setClass(time);
-    //use setClass(hour);
-}
 
 setInterval(function () {   //initializes timer
-setCurrent();  //updated fields on the hour
+    createFields();  //updated fields on the hour
 
 }, timerSet());
 
@@ -57,9 +52,11 @@ if ( i > 12 ) {
  hourDisp = i - 12;
 } else ( hourDisp = i)
 var tempId = "hour" + i;
+var buttonId = "btn" + i;
 timeDiv.text(hourDisp + ":00").addClass("hour col-2");
 fieldDiv.addClass("inputField col-8").addClass(setClass(i));
 fieldDiv.attr("id", tempId);
+saveBtn.attr("id", buttonId);  //Implement unique button ids to call
 saveBtn.addClass("saveBtn col-xs-2").text("Save");
 timePill.addClass("row");
 timePill.append(timeDiv).append(fieldDiv).append(saveBtn);
@@ -82,8 +79,8 @@ for (var i = 9; i < 18; i++) {
     $(tempId).val(toDos[j]);
 }
 }
-console.log(dateSet);
-console.log(toDos);
+// console.log(dateSet);
+// console.log(toDos);
 
 }
 
@@ -103,19 +100,43 @@ function setFields() {
     localStorage.setItem(dateSet, JSON.stringify(toDos));
 }
 
+//adding another spec for fun that pratces targeting individual buttons
+function setField() {
+    let dateSet = $("#dateSelect").val();
+    dateSet = moment(dateSet).format("DDMMYYYY");
+    toDos = JSON.parse(localStorage.getItem(dateSet));  //calls field from local to build existing array
+    if ( toDos === null) {
+        toDos =[];
+    }
+   console.log(this);
+   let tempKey = "";
+   tempKey = $(this).attr("id").slice(3);
+   let tempNum = parseInt(tempKey);
+   tempNum -= 9; //adjust array postion for hour
+   tempKey = "#hour"+ tempKey;
+   console.log(tempKey);
+   console.log($(tempKey).val());
+   toDos[tempNum] = $(tempKey).val();
+   console.log(toDos[tempNum]);
+
+localStorage.setItem(dateSet, JSON.stringify(toDos));
+}
+
+//buttons listeners
 $("#set-date-btn").on("click", function() {
     event.preventDefault();
-    $("#main").clear();
+    $("#main").empty();
     createFields();
-    getFields();
 });
 
 $("#current-date-btn").on("click", function() {
-    $(tempId).text();
+    // $(tempId).text();
     $("#dateSelect").attr("value", moment().format('MM/DD/YYYY'));
     });
 
-$(document).on("click", ".saveBtn", setFields);
+// $(document).on("click", ".saveBtn", setFields);
+
+$(document).on("click", ".saveBtn", setField);
 
 
 createFields();
